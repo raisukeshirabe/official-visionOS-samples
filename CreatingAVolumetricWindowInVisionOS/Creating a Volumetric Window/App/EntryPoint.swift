9bb67486d969790db64442208c647be3ef3f9624
raisukeshirabe <risk.srb@gmail.com>
2024-11-16 17:53:35 +0900

    Add sample code

---
+++ CreatingAVolumetricWindowInVisionOS/Creating a Volumetric Window/App/EntryPoint.swift
@@ -0,0 +1,29 @@
+/*
+See the LICENSE.txt file for this sample’s licensing information.
+
+Abstract:
+The app's main entry point
+*/
+
+import SwiftUI
+
+@main
+struct EntryPoint: App {
+    /// The multiplier for the height of the volumetric window.
+    let heightModifier: CGFloat = 0.25
+
+    var body: some Scene {
+        // Configure a window group with a volumetric window.
+        WindowGroup() {
+            VolumetricWindow()
+        }
+        .windowStyle(.volumetric)
+        // Scale the size of the window group relative to the volumetric window's size.
+        .defaultSize(
+            width: VolumetricWindow.defaultSize,
+            height: heightModifier * VolumetricWindow.defaultSize,
+            depth: VolumetricWindow.defaultSize,
+            in: .meters
+        )
+    }
+}
